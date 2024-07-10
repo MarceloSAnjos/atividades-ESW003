4c041433e6ce335dd66ce42ab147ea6b89b69e9d
Allan Yanik Hein de Carvalho <allan.yanik@gmail.com>
2024-07-10 20:37:41 -0300

    Mock front localidades

--- aula-nove/client-ultima-versao-rotas/src/localidades/Localidades.js
+++ aula-nove/client-ultima-versao-rotas/src/localidades/Localidades.js
@@ -1,8 +1,15 @@
 function Localidades() {
+  const localidadesMock = [
+    { id: 1, descricao: "pindamonhangaba" },
+    { id: 2, descricao: "barril radioativo na ZL" }
+  ];
+
+  return localidadesMock.map(localidade => {
     return (
-        <h1>Implementar</h1>
+      <li key={localidade.id}>{localidade.descricao}</li>
     )
-    
+  })
 }
+
 
 export default Localidades;
